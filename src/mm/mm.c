68d94969757353eeaa232aa733ab39f1ee3feee5
Michael Z <michael2012z@gmail.com>
2013-06-18 00:18:34 +0800

    [MM] Add some interface in mapping part.

--- src/mm/mm.c
+++ src/mm/mm.c
@@ -4,16 +4,28 @@
 #include "bootmem.h"
 #include "page_alloc.h"
 
-struct membank membank;
 unsigned long mm_pgd;
 
-void mm_init() {
-  mm_pgd = PAGE_OFFSET + PAGE_TABLE_OFFSET;
+static void map_low_memory() {
+  struct membank membank;
   membank.start = PHYS_OFFSET;
   membank.size = MEMORY_SIZE - EXCEPTION_SIZE;
   map_memory_bank(&membank);
+}
 
+void mm_init() {
+  mm_pgd = PAGE_OFFSET + PAGE_TABLE_OFFSET;
+  /* clear the page table at first*/
+  prepare_page_table();
+  /* map main memory, lowmem in linux */
+  map_low_memory();
   bootmem_initialize();
+  
+  /* map vector page */
+  map_vector_page();
+  /* map debug page */
+  map_debug_page();
+
   //  bootmem_test();
   init_pages_map();
 
