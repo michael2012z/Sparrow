5a024582eaca230a805bc2c916ee9de98d098b7e
Michael Z <michael2012z@gmail.com>
2013-07-06 21:56:54 +0800

    More merge, still OK.

---
+++ src_tmp/mm/alloc.h
@@ -0,0 +1,8 @@
+#ifndef _ALLOC_H_
+#define _ALLOC_H_
+
+#include "bootmem.h"
+#include "page_alloc.h"
+#include "slab_alloc.h"
+
+#endif /* _ALLOC_H_ */
--- src_tmp/mm/mm.c
+++ src_tmp/mm/mm.c
@@ -1,29 +1,46 @@
 #include <type.h>
+#include <memory.h>
+#include <linkage.h>
+#include <interrupt.h>
 #include "mem_bank.h"
 #include "mem_map.h"
-#include "bootmem.h"
-#include "page_alloc.h"
-#include "slab_alloc.h"
+#include "alloc.h"
+#include <mm.h>
+#include <printk.h>
 
 struct membank membank;
 unsigned long mm_pgd;
 
+extern bool boot_alloc_ready;
+extern bool page_alloc_ready;
+extern bool slab_alloc_ready;
+
 void mm_init() {
+  boot_alloc_ready = false;
+  page_alloc_ready = false;
+  slab_alloc_ready = false;
+
   mm_pgd = PAGE_OFFSET + PAGE_TABLE_OFFSET;
   membank.start = PHYS_OFFSET;
   membank.size = MEMORY_SIZE - EXCEPTION_SIZE;
   map_memory_bank(&membank);
 
   bootmem_initialize();
+  boot_alloc_ready = true;
+  printk(PR_SS_MM, PR_LVL_DBG7, "mm_init(): boot memory allocator initialized\n");
+
   //  bootmem_test();
   init_pages_map();
 
   page_alloc_init();
   //  pages_alloc_test();
+  page_alloc_ready = true;
 
   bootmem_finalize();
+  boot_alloc_ready = false;
 
   slab_alloc_init();
+  slab_alloc_ready = true;
   //  slab_alloc_test();
 
 
