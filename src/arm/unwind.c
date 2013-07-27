a082cadaaa821681228444aff567fd2161610f11
Michael Z <michael2012z@gmail.com>
2013-07-27 19:30:38 +0800

    Rewrite IRQ assembly code, succeed.

---
+++ src/arm/unwind.c
@@ -0,0 +1,15 @@
+
+
+/* Dummy functions to avoid linker complaints */
+void __aeabi_unwind_cpp_pr0(void)
+{
+};
+
+void __aeabi_unwind_cpp_pr1(void)
+{
+};
+
+void __aeabi_unwind_cpp_pr2(void)
+{
+};
+
